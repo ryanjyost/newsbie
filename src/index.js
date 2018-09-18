4febec18425e4db244563c7fa11a413931e70783
Ryan Yost <ryost217@gmail.com>
2018-09-17 23:05:14 -0500

    dotenv fixes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import MainRouter from "./MainRouter";
 import registerServiceWorker from "./registerServiceWorker";
-require("dotenv").config();
 
 ReactDOM.render(<MainRouter />, document.getElementById("root"));
 registerServiceWorker();
