fe688ed0f96487e6c6a2fee5136ab82fe28fcdba
Ryan Yost <ryost217@gmail.com>
2018-09-17 22:53:19 -0500

    dotenv

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import MainRouter from "./MainRouter";
 import registerServiceWorker from "./registerServiceWorker";
+require("dotenv").config();
 
 ReactDOM.render(<MainRouter />, document.getElementById("root"));
 registerServiceWorker();
